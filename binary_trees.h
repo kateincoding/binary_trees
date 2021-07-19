31bece833470a0f36fb3a36e453ee057224b1cf1
kateincoding <kateincoding@gmail.com>
2021-07-19 14:53:06 +0000

    structure

---
+++ binary_trees.h
@@ -0,0 +1,24 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+#endif
