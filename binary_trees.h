f8fc4dc90000b5814356949e5a116fed9869a350
kateincoding <kateincoding@gmail.com>
2021-07-20 01:02:05 +0000

    headers and adding functions

--- binary_trees.h
+++ binary_trees.h
@@ -22,7 +22,17 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
+/* print function */
+void binary_tree_print(const binary_tree_t *);
+
+/* binary tree */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
