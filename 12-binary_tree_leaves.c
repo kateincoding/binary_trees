dc6332767f97cbb91c10054d3d8f63b028acd3e1
kateincoding <kateincoding@gmail.com>
2021-07-21 13:09:20 +0000

    [betty style norme]

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	if (!(tree->left) && !(tree->right))
 		return (1);
-	
+
 	count_leaves += binary_tree_leaves(tree->left);
 	count_leaves += binary_tree_leaves(tree->right);
 
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,9 +8,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (!node)
-        return (0);
-    if (!(node->left) && !(node->right))
-        return (1);
-    return (0);
+	if (!node)
+		return (0);
+	if (!(node->left) && !(node->right))
+		return (1);
+	return (0);
 }
