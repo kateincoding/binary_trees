ec4a3da7618543665da7383ce5bfdde68131458f
rennleon <2829@holbertonschool.com>
2021-07-21 10:39:57 -0500

    Add condition to handle NULL values

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -17,8 +17,14 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 		return (tree);
 
 	chd_left = tree->left;
+
 	tree->left = chd_left->right;
+	if (tree->left)
+		tree->left->parent = tree;
+
 	chd_left->right = tree;
+	chd_left->parent = tree->parent;
+	tree->parent = chd_left;
 
 	return (chd_left);
 }
