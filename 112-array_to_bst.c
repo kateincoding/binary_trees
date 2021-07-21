e42d1a356ffa7cb2de7f000a7a0b04f6e7180d27
rennleon <2829@holbertonschool.com>
2021-07-21 11:41:11 -0500

    Create a funstion that builds a BST from an array

---
+++ 112-array_to_bst.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - Build a BST from an array
+ * @array: Pointer to an array of integers
+ * @size: Array's size
+ *
+ * Return: Pointer to the node with the searched value
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i;
+	bst_t *bst;
+
+	if (!array || size == 0)
+		return (NULL);
+
+	bst = bst_insert(&bst, array[0]);
+	for (i = 1; i < size; i++)
+		bst_insert(&bst, array[i]);
+
+	return (bst);
+}
