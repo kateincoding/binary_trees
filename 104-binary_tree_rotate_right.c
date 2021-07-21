6593025eaf87e7c423692fada6a2da40a70ab7da
rennleon <2829@holbertonschool.com>
2021-07-21 17:30:07 -0500

    Update function to handle parent child pointers update

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *chd_left;
+	binary_tree_t *chd_left, *pnode;
 
 	if (!tree)
 		return (NULL);
@@ -24,7 +24,14 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 
 	chd_left->right = tree;
 	chd_left->parent = tree->parent;
+
+	pnode = tree->parent;
 	tree->parent = chd_left;
+
+	if (pnode && pnode->left == tree)
+		pnode->left = chd_left;
+	else if (pnode && pnode->right == tree)
+		pnode->right = chd_left;
 
 	return (chd_left);
 }
