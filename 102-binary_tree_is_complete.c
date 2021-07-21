72a0da00ae7509bd757e654913e353baa5b0ccd4
rennleon <2829@holbertonschool.com>
2021-07-21 02:02:23 -0500

    Fix condition to check for NULL's

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -11,9 +11,9 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
-	if (!tree->left || tree->right)
+	if (!tree->left || !tree->right)
 		return (0);
 
 	return (binary_tree_is_complete(tree->left) &&
