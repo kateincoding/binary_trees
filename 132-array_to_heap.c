2e5f45d598454f90d5290d14eb974874fd29d966
rennleon <2829@holbertonschool.com>
2021-07-23 12:11:25 -0500

    Create a function that builds a binary heap from an array

---
+++ 132-array_to_heap.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_heap - Builds a MAx Heap from an array
+ * @array: Array of integers
+ * @size: Array's size
+ *
+ * Return: Pointer to the heap's root node
+ */
+heap_t *array_to_heap(int *array, size_t size)
+{
+	size_t i;
+	heap_t *root;
+
+	if (!array)
+		return (NULL);
+
+	root = heap_insert(&root, array[0]);
+	for (i = 1; i < size; i++)
+		heap_insert(&root, array[i]);
+
+	return (root);
+}
