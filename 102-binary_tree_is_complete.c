75edd316770c8eef9a9431c19d739a96840581ef
rennleon <2829@holbertonschool.com>
2021-07-23 09:49:28 -0500

    Add new functions to solve compute if the tress is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,6 +1,47 @@
 #include "binary_trees.h"
 
 /**
+ * count_nodes - Counts the number of nodes inside a tree
+ * @root: Pointer to tree's root node
+ *
+ * Return: Number of tree nodes
+ */
+int count_nodes(binary_tree_t *root)
+{
+	if (!root)
+		return (0);
+
+	return (1 + count_nodes(root->left) + count_nodes(root->right));
+}
+
+
+/**
+ * is_complete - Checks if a tree is complete
+ * @root: Pointer to tree's root
+ * @index: Index of the node been evaluated
+ * @n: number of trees nodes
+ *
+ * Return: 1 if the tree is a heap, 0 otherwise
+ */
+int is_complete(binary_tree_t *root, int index, int n)
+{
+	if (!root)
+		return (0);
+
+	if (index >= n)
+		return (0);
+	if (!root->left && !root->right)
+		return (1);
+	if (root->right && !root->left)
+		return (0);
+	if (root->left && !root->right)
+		return (is_complete(root->left, index * 2 + 1, n));
+
+	return (is_complete(root->left, index * 2 + 1, n) &&
+		is_complete(root->right, index * 2 + 2, n));
+}
+
+/**
  * binary_tree_is_complete - Checks if a binary tree is complete
  * @tree: Pointer to trees' root
  *
@@ -8,14 +49,14 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+	int nodes;
+	binary_tree_t *root;
+
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
-	if (!tree->left || !tree->right)
-		return (0);
+	root = (binary_tree_t *)tree;
+	nodes = count_nodes(root);
 
-	return (binary_tree_is_complete(tree->left) &&
-		binary_tree_is_complete(tree->right));
+	return (is_complete(root, 0, nodes));
 }
