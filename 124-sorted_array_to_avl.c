4c8a72c8bd676a6662aaf4b281a8533a314a5ccd
Katherine Soto <kateincoding@gmail.com>
2021-07-23 17:26:29 -0400

    sorted array to avl

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+
+avl_t *recursion_sorted_array(int *array, int first_idx, int last_idx)
+{
+	avl_t *root;
+	int middle;
+
+	if (last_idx < first_idx)
+		return (NULL);
+
+	middle = (last_idx + first_idx) / 2;
+	/* creation of binary tree with root = middle */
+	root = binary_tree_node(NULL, array[middle]);
+	if (!root)
+		return (NULL);
+	/* sort each branch */
+	root->left = recursion_sorted_array(array, first_idx, middle - 1);
+	root->right = recursion_sorted_array(array, middle + 1, last_idx);
+	if (root->right)
+		root->right->parent = root;
+	else if (root->left)
+		root->left->parent = root;
+	return (root);
+}
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array || size < 1)
+		return (NULL);
+	return (recursion_sorted_array(array, 0, size - 1));
+}
